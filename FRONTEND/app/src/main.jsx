4e0fbf62ed851533df38c4501372c0f151f48e7c
Muhammed Halid Kutsal <mhalidkutsal@gmail.con>
2024-01-11 15:08:14 +0100

    submit button width

--- FRONTEND/app/src/main.jsx
+++ FRONTEND/app/src/main.jsx
@@ -6,6 +6,6 @@ import SignIn from './components/SignIn/SignIn.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
 )
