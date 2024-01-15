a5fc115bca609e84e0084cdc0f2e05d13e3f2fff
Squasmn <Squasmn.withem@gmail.com>
2024-01-15 12:35:46 +0100

    minor fix in main.jsx

--- FRONTEND/app/src/main.jsx
+++ FRONTEND/app/src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import SignIn from './components/SignIn/SignIn.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SignIn />
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
